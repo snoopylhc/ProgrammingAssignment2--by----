f90cfe06c0bd31765fa7c028c5b39548c04201da
snoopylhc <xinxianglhc@126.com>
2014-07-27 23:26:52 +0800

    Update and rename .gitignore to cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,38 @@
+## These functions are attempt to cache the inverse of a matrix in order to
+## reduce the repeatly calculation of inversing.
+
+## "makeCacheMatrix()" is a function that creates a special "matrix" object 
+## that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setsolve<- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+}
+
+
+## "cacheSolve()" is a function that computes the inverse of the special 
+## "matrix" returned by makeCacheMatrix above. If the inverse has already 
+## been calculated (and the matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
+}
